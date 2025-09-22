b80edc029874ee4807cb979470485a5cee3afc50
Bumlistic <144963901+Bumlistic@users.noreply.github.com>
2025-09-22 00:09:51 -0700

    Create backend.tf

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = var.devflow_resource_group_name
+    storage_account_name = var.devflow_storage_account_name
+    container_name       = var.devflow_container_name
+    key                   = var.devflow_state_key
+    use_azuread_auth      = true
+  }
+}
